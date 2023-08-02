6a8ae14324ae5e169f87fd74fff70b8192d690df
Volodia99 <gaylor.wafflard@univ-grenoble-alpes.fr>
2023-08-02 18:02:12 +0200

    use different constructor for GasDataSet (from_npy classmethod), using the reduced npy files instead of the original outputs

--- tests/test_load_data.py
+++ tests/test_load_data.py
@@ -14,3 +14,19 @@ def test_api_vtk_by_name(test_data_dir):
         FileNotFoundError, match="In idfxReadVTK: datawrong.0500.vtk not found."
     ):
         GasDataSet(500, pattern=lambda on: f"datawrong.{on:04d}.vtk")
+
+
+def test_from_npy(test_data_dir):
+    os.chdir(test_data_dir / "idefix_spherical_planet3d")
+
+    with pytest.raises(
+        FileNotFoundError, match="Original output was not reduced, or file"
+    ):
+        GasDataSet.from_npy(500, operation="azimuthal_averag")
+
+    ds = GasDataSet(500)
+    assert len(list(ds.keys())) == 7
+    ds["RHO"].azimuthal_average().save()
+
+    dsnpy = GasDataSet.from_npy(500, operation="azimuthal_average")
+    assert len(list(dsnpy.keys())) == 1
